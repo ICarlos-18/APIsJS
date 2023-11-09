07051d98a754f27a98ff682740acc2ff6e8af452
ICarlos-s18 <csanchezhernandez567@gmail.com>
2023-11-08 21:08:17 -0600

    actualización

--- RickandMorty/app.js
+++ RickandMorty/app.js
@@ -1,40 +1,41 @@
-import { createCrad, createCardModal, createPag } from "./ui.js";
+import { createCard, createCardModal, createPag } from './ui.js';
 
 const URL = 'https://rickandmortyapi.com/api';
 const containerChar = document.querySelector('.characters');
 const pagination = document.querySelector('.pagination');
 const modalTitle = document.querySelector('.modal-title');
 const modalBody = document.querySelector('.modal-body');
-
 const fetchApi = (url) => fetch(url).then(response => response.json());
 
-pagination.innerHTML = createPag();c
+pagination.innerHTML = createPag();
 
 function getCharacters(page=1){
-    fetchApi(`${ URL }/character/?page=${ page }`)
-    .then(data => {
-        const characters = data.results;
-        showCharacters(characters);
-    })
-}
-function getCharactersById(id){
-    fetchApi(`${ URL }/character/${ id }`)
-    .then(data => {
-        const character = data;
-        modalTitle.innerHTML = character.name;
-        modalBody.innerHTML = '';
-        modalBody.appendChild(createCardModal(character));
-    })
+fetchApi(`${ URL }/character/?page=${ page }`)
+      .then(data => {
+          const characters = data.results;
+          showCharaters(characters);
+      });
 }
 
-function showCharacters(characters){
+function getCharactesById(id){
+  fetchApi(`${ URL }/character/${ id }`)
+      .then(data => {
+          const character = data;
+          //console.log(character);
+          modalTitle.innerHTML = character.name;
+          modalBody.innerHTML = '';
+          modalBody.appendChild(createCardModal(character));
+      });
+}
+
+function showCharaters(characters){
     containerChar.innerHTML = '';
     characters.forEach(character => {
-        containerChar.appendChild(createCrad(character));
-    });
+        containerChar.appendChild(createCard(character));
+    })
 }
 
-function getButton(e){
+function getPag(e){
     e.preventDefault();
     if(e.target.classList.contains('page-link')){
         const page = e.target.getAttribute('data-id');
@@ -42,16 +43,17 @@ function getButton(e){
     }
 }
 
-function getButtonCard(e){
+function getbuttonCard(e){
     e.preventDefault();
     if(e.target.classList.contains('btn')){
         const id = e.target.getAttribute('data-id');
-        getCharactersById(id);
+        getCharactesById(id);
     }
 }
 
-getCharacters();
-createPag();
 
-pagination.addEventListener('click', getButton);
-containerChar.addEventListener('click', getButtonCard);
+createPag();
+getCharacters();
+
+pagination.addEventListener('click', getPag);
+containerChar.addEventListener('click', getbuttonCard);
